dea18b7e916b8cc7e4a59b7b405cd2e85a4503ec
arianalima10 <ariana.crodrigues@gmail.com>
2025-10-05 13:12:22 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,7 +41,11 @@ public class Program {
 				
 				if (chessMatch.getPromoted() != null) { // Alguma peça foi promovida
 					System.out.print("Enter piece for promotion (B/N/R/Q): "); // Jogador indica para qual tipo de peça a peça Peão foi promovido
-					String type = sc.nextLine();
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					chessMatch.replacePromotedPiece(type); // Faz a troca de Peão para o tipo informado pelo jogador
 				}
 			}
